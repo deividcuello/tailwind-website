9b82a099b051c0a5d7d11c87da72de0c07fa6ed0
unknown <deividcuellocuevas@gmail.com>
2023-06-17 12:37:33 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ module.exports = {
       green: '#023020',
       lightGreen: '#00FF00',
       lightRed: '#FF0000',
-      lightGold: '#FFD700'
+      lightGold: '#FFD700',
+      yellow: '#FFFF00'
+    },
+    screens: {
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
     },
     extend: {
       fontFamily: {
